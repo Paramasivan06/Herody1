7f6885f9ae33065e3f5a217a1b1a7c77884cec0d
Paramasivan06 <parama@herody.in>
2025-03-15 11:11:32 +0530

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -220,7 +220,7 @@
     <!--====== Start Header ======-->
     <header class="template-header absolute-header navbar-left sticky-header">
         <div class="topbar">
-            <p >Let's make something great togetherss.</p>
+            <p >Let's make something great together.</p>
         </div>
         <div class="container">
             <div class="header-inner">
